4cdedc443d10521ed5fb4fe3a238c38cc4feb032
Alina Lemishko <aliska.nofear@gmail.com>
2022-07-07 13:07:47 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Searchbar from 'components/Searchbar/Searchbar';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 
-class App extends Component {
-  state = {
-    searchImg: '',
+export default function App() {
+  const [searchImg, setSearchImg] = useState('');
+
+  const handleSubmit = image => {
+    setSearchImg(image);
   };
 
-  handleSubmit = image => {
-    this.setState({ searchImg: image });
-  };
-
-  render() {
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery searchImg={this.state.searchImg} />
-        {/*  <Loader /> */}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery searchImg={searchImg} />
+    </>
+  );
 }
-
-export default App;
